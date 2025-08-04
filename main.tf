4f24d2544ea92194c16baf4b6115dc1136afd9cf
nezort11 <nezort11@gmail.com>
2025-08-04 22:51:40 +0300

    chore: Increase shorts forward timeout duration

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "yandex_function" "shorts-forward-function" {
   service_account_id = var.service_account_id
 
   memory = 2048
-  execution_timeout = 180
+  execution_timeout = 300
   concurrency = 3
 
   content {
